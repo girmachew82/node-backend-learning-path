1e353eb92fd87aa37f0d3eef95f55f9324e9fe38
girmachew82 <girmachewabebe@gmail.com>
2026-04-09 16:05:51 +0300

    feat: node event loop and callback example

--- phase-1-core-node/example.js
+++ phase-1-core-node/example.js
@@ -3,3 +3,19 @@ setTimeout(() => {
 }, 2000);
 
 console.log('Start');
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+  sum += i;
+}
+console.log('Sum:', sum);
+
+
+sum = 0;
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) {
+    sum += i
+  }
+  continue
+}
+console.log('Sum:', sum);
